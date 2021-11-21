dc71c97c4c7b99db89163cfac3e95a10c3cd0d7c
ahoang07 <ahoang07@uw.edu>
2021-11-21 15:55:08 -0800

    added school-survey folder with csvs

---
+++ summaryinfoscript.R
@@ -0,0 +1,8 @@
+# Load the tidyverse package
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
+# Load the *enrollment_trends* data into a variable. `enrollment_trends` 
+enrollment_trends <- read.csv("school-survey/01_data/011_rawdata/enrollment.csv")
+
