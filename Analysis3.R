2fd1b83c5b009f6756cf6b5d9465f3410e35484d
August Cooke <aecooke@uw.edu>
2021-11-22 21:58:43 -0800

    analysis3 added

---
+++ Analysis3.R
@@ -0,0 +1,54 @@
+library(tidyverse)
+library(stringr)
+library(dplyr)
+library(plotly)
+library(ggplot2)
+library(maps)
+library(usdata)
+library(leaflet)
+
+
+testingpolicies <- read.csv(file = "covid-19-testing-policy.csv", stringsAsFactors = FALSE)
+View(testingpolicies)
+
+#country <- testingpolicies %>%
+#  filter(Entity == "China")
+
+#country %>% 
+#  group_by(testing_policy) %>%
+# summarise(Day = n())
+
+#View(country)
+
+#daysofpolicies <- ggplot(country, aes(x ="", y = testing_policy, fill = Day)) +
+#  geom_col() +
+#  coord_polar(theta = "y")
+
+#ggplotly(daysofpolicies)
+
+country <- testingpolicies %>%
+  filter(testing_policy == "3") %>% 
+  group_by(Entity) %>%
+  summarise(Day = n())
+
+View(country)
+
+#daysofpolicies <- ggplot(country) +
+#  geom_polygon(
+#   mapping = aes(x = long, y = lat, group = group, fill = testing_policy),
+#   color = "white",
+#    size = .1) +
+#  coord_map() +
+#  labs(fill = "testing_policy") +
+#  ggtitle("Countries testing policy levels") +
+#  theme()
+
+#daysofpolicies  
+
+
+daysofpolicies <- ggplot(country, aes(x = Entity, y = Day)) +
+  geom_point()
+
+
+ggplotly(daysofpolicies)
+
