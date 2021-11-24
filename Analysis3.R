9531785afba966139e5b0bce919adda54c5fd2d3
August Cooke <aecooke@uw.edu>
2021-11-23 23:41:25 -0800

    NEW GRAPH

--- Analysis3.R
+++ Analysis3.R
@@ -8,49 +8,25 @@ library(usdata)
 library(leaflet)
 
 
-testingpolicies <- read.csv(file = "covid-19-testing-policy.csv", stringsAsFactors = FALSE)
-View(testingpolicies)
+library(readr)
+covid_19_testing_policy <- read_csv("covid-19-testing-policy.csv")
+View(covid_19_testing_policy)
 
-#country <- testingpolicies %>%
-#  filter(Entity == "China")
-
-#country %>% 
-#  group_by(testing_policy) %>%
-# summarise(Day = n())
-
-#View(country)
-
-#daysofpolicies <- ggplot(country, aes(x ="", y = testing_policy, fill = Day)) +
-#  geom_col() +
-#  coord_polar(theta = "y")
-
-#ggplotly(daysofpolicies)
-
+# FILTER COUNTRIES WITH HIGHEST TESTING POLICY FOR LONGEST TIME
 country <- testingpolicies %>%
-  filter(testing_policy == "3") %>% 
+  filter(testing_policy == "3") %>%
   group_by(Entity) %>%
-  summarise(Day = n())
-#sort by day, increasing
+  summarise(Day = n()) %>%
+  top_n(10)
 
-View(country)
-
-#daysofpolicies <- ggplot(country) +
-#  geom_polygon(
-#   mapping = aes(x = long, y = lat, group = group, fill = testing_policy),
-#   color = "white",
-#    size = .1) +
-#  coord_map() +
-#  labs(fill = "testing_policy") +
-#  ggtitle("Countries testing policy levels") +
-#  theme()
-
-#daysofpolicies  
-
-
-daysofpolicies <- ggplot(country, aes(x = Entity, y = Day)) +
-  geom_point() +
-  ggtitle("Countries testing policy levels")
-
-
-ggplotly(daysofpolicies)
+# PIE CHART
+daysofpolicies <- plot_ly(country, labels = ~Entity, 
+                          values = ~Day, type = "pie")
+daysofpolicies <- daysofpolicies %>%
+  layout(
+    title = "Countries testing policy levels",
+    xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+    yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE)
+  )
+daysofpolicies
 
