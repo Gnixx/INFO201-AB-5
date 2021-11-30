cefe51e856e6c56347b0e3a13a8986637ce7fb66
Gnixx <rena.bonnan@gmail.com>
2021-11-30 13:31:58 -0800

    folder

---
+++ ExploratoryAnalysis/Analysis3.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(stringr)
+library(dplyr)
+library(plotly)
+library(ggplot2)
+library(maps)
+library(usdata)
+library(leaflet)
+
+
+library(readr)
+testingpolicies <- read_csv("covid-19-testing-policy.csv")
+
+
+# FILTER COUNTRIES WITH HIGHEST TESTING POLICY FOR LONGEST TIME
+country <- testingpolicies %>%
+  filter(testing_policy == "3") %>%
+  group_by(Entity) %>%
+  summarise(Day = n()) %>%
+  top_n(10)
+
+# PIE CHART
+daysofpolicies <- plot_ly(country, labels = ~Entity, 
+                          values = ~Day, type = "pie")
+daysofpolicies <- daysofpolicies %>%
+  layout(
+    title = "Countries testing policy levels",
+    xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+    yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE)
+  )
+daysofpolicies
+
+
---
+++ ExploratoryAnalysis/map.R
@@ -0,0 +1,52 @@
+
+library(tidyverse)
+library(stringr)
+library(dplyr)
+library(ggplot2)
+library(wesanderson)
+library(usmap)
+
+# Loading data -------------------------------------------------------------------------
+
+Colleges <- read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/colleges/colleges.csv")
+source("./analysis2.R")
+
+# Map -------------------------------------------------------------------------
+
+blank_theme <- theme_bw() +
+  theme(
+    axis.line = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title = element_blank(),
+    plot.background = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank()
+  )
+
+# aapi_state <- incarceration_trends %>%
+#   group_by(state) %>%
+#   filter(year == max(year, na.rm = T)) %>%
+#   filter(aapi_jail_pop == aapi_jail_pop) %>%
+#   filter(total_pop == total_pop) %>%
+#   mutate(total_pop / aapi_jail_pop) %>%
+#   summarize(
+#     pop = sum(aapi_jail_pop), total = max(total_pop),
+#     mutate = sum(total_pop / aapi_jail_pop)
+#   )
+
+
+cases_map <- plot_usmap(
+  data = College_cases, values = "state_sum", color = "black"
+) +
+  blank_theme +
+  scale_fill_gradientn(
+    colours = c("white", "brown"),
+    breaks = c(10, 100, 1000, 10000, 100000),
+    trans = "log10", name = "number of cases"
+  ) +
+  labs(title = "Covid-19 Cases in United States' Colleges and Universities") +
+  theme(legend.position = "right")
+
+cases_map
