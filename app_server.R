d2f05b8777d7004ff78cb46e0acb656749be642a
Gnixx <rena.bonnan@gmail.com>
2021-12-09 03:27:44 -0800

    upload timeline

--- app_server.R
+++ app_server.R
@@ -1,32 +1,74 @@
-#app server
+# app server
 library(shiny)
 library(plotly)
 library(dplyr)
 library(tidyverse)
 library(ggplot2)
-
 library(readr)
-testingpolicies <- read_csv("covid-19-testing-policy.csv")
 
+## Read in data
+df <- read.csv("docs/Vaccination.csv") %>%
+  mutate(Year = str_sub(Date.Administered, 7, 10)) %>%
+  mutate(Month = str_sub(Date.Administered, 1, 2)) %>%
+  mutate(Date = str_sub(Date.Administered, 1, 5)) %>%
+  filter(Year >= "2021") %>%
+  rename(
+    AgeGroup = AgeGroupVacc,
+    WeeklyCasesPer100k = X7.day_avg_group_cases_per_100k,
+    OneDose = Administered_Dose1_pct_agegroup,
+    FullyVaccinated = Series_Complete_Pop_pct_agegroup
+  )
+df$Date.Administered <- NULL
+df$Date <- as.Date(df$Date, format = "%m/%d")
+
+testingpolicies <- read_csv("docs/covid-19-testing-policy.csv")
+
+
+
+## Start shinyServer
 server <- function(input, output) {
-  
+  ## Timeline
+  output$Timeline <- renderPlotly({
+    data <- df %>%
+      filter(AgeGroup == input$age) %>%
+      select(Date, OneDose, FullyVaccinated) %>%
+      gather(key = Vacc, value = percentage, -Date)
+
+
+    p <- ggplot(data, aes(
+      x = Date, y = percentage / 100,
+      color = Vacc
+    )) +
+      geom_line() +
+      scale_y_continuous(labels = scales::percent) +
+      labs(
+        title = paste("COVID-19 Vaccination Trends in", input$age),
+        x = "Date", y = "Percent of population"
+      ) +
+      scale_color_manual(values = c("orange4", "orange"))
+
+    fig <- ggplotly(p)
+    return(fig)
+  })
+
+  ## pie
   output$pie <- renderPlotly({
-    
     country <- testingpolicies %>%
       filter(testing_policy == "3") %>%
       group_by(Entity) %>%
       summarise(Day = n()) %>%
-      sample_n(15)#input$countrychoice)
-    
-    pieplot <- plot_ly(country, labels = ~Entity, 
-                       values = ~Day, type = "pie") %>%
+      sample_n(15) # input$countrychoice)
+
+    pieplot <- plot_ly(country,
+      labels = ~Entity,
+      values = ~Day, type = "pie"
+    ) %>%
       layout(
         title = "Countries testing policy levels",
         xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
         yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE)
       )
-    
     pieplot
-    #return(pieplot)
+    # return(pieplot)
   })
 }
--- app_ui.R
+++ app_ui.R
@@ -6,23 +6,52 @@ introductionTab <- tabPanel(
   "Introduction",
   titlePanel("Introduction"),
   h3("(Name of Project)"),
-  p("Due to  the school closures during COVID-19, over a billion children are at 
-    risk of falling behind in school. To keep students safe, countries have implemented 
-    virtual learning and we believe that this change has affected countless students. 
-    Our group wanted to explore this domain more, as we are all students pursuing higher 
-    education. We tried to look at the impacts of COVID-19 on the global education 
+  p("Due to  the school closures during COVID-19, over a billion children are at
+    risk of falling behind in school. To keep students safe, countries have implemented
+    virtual learning and we believe that this change has affected countless students.
+    Our group wanted to explore this domain more, as we are all students pursuing higher
+    education. We tried to look at the impacts of COVID-19 on the global education
     system to see if there were any national trends correlated with Covid-19."),
   p("For this project, we looked at three datasets. They are:"),
-  p("National Education Responses to COVID-19 School Closures", a(href="..", "Click here for data!")), 
-  p("COVID-19 Vaccination and Case Trends by Age Group, United States", a(href="..", "Click here for data!")),
-  p("COVID-19 Testing Policities Tracking Covid-19 at U.S. Collegtes and Universities", a(href="..", "Click here for data!")),
+  p("National Education Responses to COVID-19 School Closures", a(href = "..", "Click here for data!")),
+  p("COVID-19 Vaccination and Case Trends by Age Group, United States", a(href = "..", "Click here for data!")),
+  p("COVID-19 Testing Policities Tracking Covid-19 at U.S. Collegtes and Universities", a(href = "..", "Click here for data!")),
   p(""),
   p("The total school enrollment in 2020 was")
 )
 
-testingpolicies <- read_csv("covid-19-testing-policy.csv")
+
+# chart panel
+## chart sidebar
+chart_sidebar <- sidebarPanel(
+  age_input <- selectInput(
+    inputId = "age",
+    label = "Age Group",
+    choices = list(
+      "<12 Years" = "<12 Years",
+      "12 - 15 Years" = "12 - 15 Years",
+      "16 - 17 Years" = "16 - 17 Years",
+      "18 - 24 Years" = "18 - 24 Years"
+    )
+  )
+)
+
+## chart mainbar
+chart_main <- mainPanel(
+  plotlyOutput("Timeline")
+)
+
+timeline_panel <- tabPanel(
+  "Timeline",
+  titlePanel("COVID-19 Vaccination and Case Trends by Age Group, United States"),
+  chart_sidebar,
+  chart_main
+)
+
+
+# Pie Chart
+testingpolicies <- read_csv("docs/covid-19-testing-policy.csv")
 pie_sidebar_content <- sidebarPanel(
-  
   selectInput(
     label = "choose country",
     choices = country$Entity,
@@ -38,9 +67,7 @@ pie_panel <- tabPanel(
   "Pie Chart",
   # titlePanel("Countries with highest level testing policy; by time"),
   sidebarLayout(
-    
     pie_sidebar_content,
-    
     pie_main_content
   )
 )
@@ -55,16 +82,16 @@ conclusionTab <- tabPanel(
   with a higher number of school closures within a certain geographic area.
   However, it was more widespread than me thought. Since China's school closures
   were within the top 20 countries, we expected more East Asian countries
-  in the top 20 countries with school closures than were actually listed 
+  in the top 20 countries with school closures than were actually listed
   in our chart."),
-  p("A second takeaway coming from the Vaccinations dataset was 
+  p("A second takeaway coming from the Vaccinations dataset was
   more people in both age groups 12-17 and 18-24 had a higher percentage of
   the population taking Dose 1 rather than completing both doses. Within these
   two age groups, it surprised us that the group ages 12-17 had a higher
   percetage of the population getting their first dose."),
   p("A takeaway we had from the Covid-19 testing policy dataset
   was how long testing policy 3 was available to people and how Covid-19
-  tests were becoming more readily available to the general population overtime. 
+  tests were becoming more readily available to the general population overtime.
   Testing policy 3 allowed for more people to gain access to Covid-19 testing
   which also had an effect on reopenings of schools for each country. However,
   a lot of countries like Austrailia still had more limited testing
@@ -75,6 +102,7 @@ conclusionTab <- tabPanel(
 ui <- navbarPage(
   "Final Deliverable",
   introductionTab,
+  timeline_panel,
   pie_panel,
   conclusionTab,
 )
