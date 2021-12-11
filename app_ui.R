36908df5260f278e6e3fac370f7650604b022332
ahoang07 <ahoang07@uw.edu>
2021-12-10 20:02:16 -0800

    chnages to intro and conclusion

--- app_ui.R
+++ app_ui.R
@@ -13,23 +13,17 @@ introductionTab <- tabPanel(
     Our group wanted to explore this domain more, as we are all students pursuing higher
     education. We tried to look at the impacts of COVID-19 on the global education
     system to see if there were any national trends correlated with Covid-19."),
-  p("There are some examples of data drive project that we found related to this domain:"),
-  tags$ul(
-    tags$li("Unesco collected data monitoring school closures globally due to
-            COVID-19 and their impact on learners"),
-    tags$li("Another study conducted by Unesco looked at the total duration of school
-            closures during the pandemic and how this number"),
-    tags$li("A study by the Economic Policy Institute researched how learning and online
-            instruction during the pandemic has affected engagement, teaching, and learning"),
-    tags$li("The CDC did a study on vaccination coverage among adolescents from the ages 13-17")
-  ),
   p("There are some data-driven questions we wish to answer:"),
   tags$ul(
-    tags$li("What is the total number of the affected students?"),
-    tags$li("What is the percentage of students unenrolled?"),
     tags$li("How did students perform during school closures?"),
     tags$li("How many students are unable to set up online learning platforms? Why?"),
-    tags$li("How did changing COVID-19 testing policies affect the number of cases in the US?")
+    tags$li("How did changing COVID-19 testing policies affect the number of cases in the US?"),
+    tags$li("Did the amount of time that a country held a certain COVID-19 testing policy
+            have an association with the number of school closures there were?"),
+    tags$li("Did the amount of time that a country held a certain COVID-19 testing policy
+            have an association with the number of school closures there were?"),
+    tags$li("Which age group had a higher percentage of people who took both doses
+            of the vaccination?"),
   ),
   p(),
   p("We studied three datasets in this project. They are:"),
@@ -46,10 +40,7 @@ introductionTab <- tabPanel(
     p("This dataset answers a few questions:"),
     tags$ul(
       tags$li("What is the longest duration of time that students did online learning?"),
-      tags$li("What country has the lowest number of affected students?"),
-      tags$li("What country has the highest number of affected students?"),
-      tags$li("What did schools do in response to school closures and student performance?"),
-      tags$li("What did we learn from these datasets?")
+      tags$li("Which countries had the longest duration of school closures?"),
     ),
     p(),
     tags$li(
@@ -64,34 +55,12 @@ introductionTab <- tabPanel(
     p("This dataset answers a few questions:"),
     tags$ul(
       tags$li("How did the situation of vaccination affect the recovery of education?"),
-      tags$li("How did the number of new cases affect the recovery of education?")
-    ),
-    p(),
-    tags$li(
-      "COVID-19 Testing Policities Tracking Covid-19 at U.S. Collegtes and Universities",
-      a(href = "https://github.com/nytimes/covid-19-data/tree/master/colleges", "Click here for data!")
-    ),
-    p("Data was collected by Oxford volunteers across the world, and logged continuously
-      into the OxCGRT (oxford covid govt response tracker)."),
-    p("The data contains lots of information pertaining to government’s response to Covid-19,
-      including covid testing availability, school closures, travel restriction and
-      vaccination rates and availability"),
-    p("This dataset answers a few questions:"),
-    tags$ul(
-      tags$li("How countries rank with combined restrictions?"),
-      tags$li("What level of specific restrictions each country has?"),
-      tags$ul(
-        tags$li("School and workplace closures, (fully shut down, some open, all open)"),
-        tags$li("Internal and external travel"),
-        tags$li("Restrictions on gathering"),
-        tags$li("Stay at home order, face coverings (recommended to required)")
-      )
+      tags$li("How did the number of new cases affect the recovery of education?"),
+      tags$li("Did age group 12-17 or 18-24 have a higher percentage of the population
+              who got Dose 1 of their Covid-19 vaccination?"),
+     )
     )
-  ),
-  p(),
-  p()
-)
-
+  )
 
 # chart panel
 ## chart sidebar
@@ -163,7 +132,7 @@ conclusionTab <- tabPanel(
   collected is not always what you expect it to be. For example, looking at
   the top 20 countries with school closures, we were looking for any patterns
   with a higher number of school closures within a certain geographic area.
-  However, it was more widespread than me thought. Since China's school closures
+  However, it was more widespread than we thought. Since China's school closures
   were within the top 20 countries, we expected more East Asian countries
   in the top 20 countries with school closures than were actually listed
   in our chart."),
@@ -171,14 +140,17 @@ conclusionTab <- tabPanel(
   more people in both age groups 12-17 and 18-24 had a higher percentage of
   the population taking Dose 1 rather than completing both doses. Within these
   two age groups, it surprised us that the group ages 12-17 had a higher
-  percetage of the population getting their first dose."),
+  percentage of the population getting their first dose."),
   p("A takeaway we had from the Covid-19 testing policy dataset
   was how long testing policy 3 was available to people and how Covid-19
   tests were becoming more readily available to the general population overtime.
   Testing policy 3 allowed for more people to gain access to Covid-19 testing
   which also had an effect on reopenings of schools for each country. However,
   a lot of countries like Austrailia still had more limited testing
-  options overtime.")
+  options overtime. One finding that we found surprising was that France was one
+  of the countries that had one of the longest school closures, while also
+  having high test policies. This means that having more access to Covid 
+  testing didn't necessarily guarentee short school closures.")
 )
 
 # ui
